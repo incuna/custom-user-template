24a468225f104ccf02cb2b69e9f4510105cf06dd
Max Peterson <max@incuna.com>
2014-01-20 12:53:26 +0000

    Whitespace.

--- users/admin.py
+++ users/admin.py
@@ -23,12 +23,14 @@ class UserAdmin(BaseUserAdmin):
             'fields': ('last_login', 'date_joined')
         }),
     )
+
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
             'fields': ('email', 'name', 'password1', 'password2')}
         ),
     )
+
     list_display = ('email',)
     list_filter = ('is_active',)
     search_fields = ('name', 'email')
