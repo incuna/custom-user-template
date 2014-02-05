870464705dd94ac5c7137bf5229daff92f784870
Charlie Denton <charlie@meshy.co.uk>
2014-02-05 13:40:59 +0000

    Use of variables

--- users/forms.py
+++ users/forms.py
@@ -44,11 +44,11 @@ class UserFormBase(forms.ModelForm):
         password1 = cleaned_data.get('password1')
         password2 = cleaned_data.get('password2')
         if password1 and password2 and password1 != password2:
-            self.errors['password2'] = self.error_class([self.error_messages['password_mismatch']])
-            self.errors['password1'] = self.error_class([self.error_messages['password_mismatch']])
+            msg = self.error_messages['password_mismatch']
+            self.errors['password2'] = self.error_class([msg])
+            self.errors['password1'] = self.error_class([msg])
             del cleaned_data['password2']
             del cleaned_data['password1']
-
         return cleaned_data
 
     def save(self, commit=True):
